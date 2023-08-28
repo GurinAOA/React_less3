9ee562029da5e4b3830a3e6fe8117fae059d0461
GurinAOA <kiulo.oksana@gmail.com>
2023-08-28 15:26:33 +0300

    rewrite basket in one component

--- src/components/ItemBasket/ItemBasket.jsx
+++ src/components/ItemBasket/ItemBasket.jsx
@@ -1,64 +1,67 @@
-import React from "react";
-import s from "./ItemBasket.module.scss";
-import { useState } from "react";
-import basket from "../data/basket.json";
+// import React from "react";
+// import s from "./ItemBasket.module.scss";
+// // import { useState } from "react";
+// // import Basket from "../Basket/Basket";
+// // import basketJson from "../data/basket.json";
 
-export default function ItemBasket(props) {
-  const [cardsBasket, setCardsBasket] = useState(basket);
-  function minus(id) {
-    const arr = cardsBasket.map((item) => {
+// export default function ItemBasket(props) {
+//   console.log(props.object);
+  
+//   function minus(id) {
+//     const arr = props.object.basketJson.map((item) => {
 
-      if (item.id === id ) {
-        item.number -= 1;
-        return item;
-      }
-      return item;
-    });
-    setCardsBasket(arr);
-  }
-  function plus(id) {
-    const arr = cardsBasket.map((item) => {
-      if (item.id === id ) {
-        item.number += 1;
-        return item;
-      }
-      return item;
-    });
-    setCardsBasket(arr);
+//       if (item.id === id ) {
+//         item.number -= 1;
+//         return item;
+//       }
+//       return item;
+//     });
+//     props.object.basketJson.setCardsBasket(arr);
+//   }
+//   function plus(id) {
+//     const arr =props.object.basketJson.map((item) => {
+//       if (item.id === id ) {
+//         item.number += 1;
+//         return item;
+//       }
+//       return item;
+//     });
+//     props.object.basketJson.setCardsBasket(arr);
    
-  }
-  return (
-    <div className={s.basket_wrap}>
-      <img
-        className={s.img}
-        src={require(`../Main/images/${props.object.url}.png`)}
-        alt="food"
-      />
-      <div className={s.wr_bags_text}>
-        <p className={s.description}>{props.object.description}</p>
-        <p className={s.weight}>{props.object.weight}</p>
-        <p className={s.price}>{props.object.price}</p>
-      </div>
-      <div className={s.wr_bags_btn_container}>
-        <button
-          onClick={() => {
-            if(props.object.number>0)
-            minus(props.object.id);
-          }}
-          className={s.bags_btn}
-        >
-          -
-        </button>
-        <span className={s.number}>{props.object.number}</span>
-        <button
-          onClick={() => {
-            plus(props.object.id);
-          }}
-          className={s.bags_btn}
-        >
-          +
-        </button>
-      </div>
-    </div>
-  );
-}
+   
+//   }
+//   return (
+//     <div className={s.basket_wrap}>
+//       <img
+//         className={s.img}
+//         src={require(`../Main/images/${props.object.url}.png`)}
+//         alt="food"
+//       />
+//       <div className={s.wr_bags_text}>
+//         <p className={s.description}>{props.object.description}</p>
+//         <p className={s.weight}>{props.object.weight}</p>
+//         <p className={s.price}>{props.object.price}</p>
+//       </div>
+//       <div className={s.wr_bags_btn_container}>
+//         <button
+//           onClick={() => {
+//             if(props.object.number>0)
+//             minus(props.object.id);
+//           }}
+//           className={s.bags_btn}
+//         >
+//           -
+//         </button>
+//         <span className={s.number}>{props.object.number}</span>
+//         <button
+//           onClick={() => {
+//             plus(props.object.id);
+//           }}
+//           className={s.bags_btn}
+//         >
+//           +
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
