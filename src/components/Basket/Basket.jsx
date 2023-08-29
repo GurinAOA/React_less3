d06f2524e627bcca9c48690df214a15f8eafbabf
GurinAOA <kiulo.oksana@gmail.com>
2023-08-29 22:27:31 +0300

    change basket in two components

--- src/components/Basket/Basket.jsx
+++ src/components/Basket/Basket.jsx
@@ -2,111 +2,64 @@ import React from "react";
 import s from "../Basket/basket.module.scss";
 import basketJson from "../data/basket.json";
 import delivery from "../Main/images/free-icon-delivery-2362252.svg";
-// import ItemBasket from "../ItemBasket/ItemBasket";
+import ItemBasket from "../ItemBasket/ItemBasket";
+
 
 import { useState } from "react";
 
 export default function Basket() {
-  const [cardsBasket, setCardsBasket] = useState(basketJson);
+  const [basket, setBasket] = useState(basketJson);
 
-  function minus(id) {
-    const arr = cardsBasket.map((item) => {
-      if (item.id === id) {
-        item.number -= 1;
-        return item;
-      }
-      return item;
-    });
-    setCardsBasket(arr);
-  }
-  function plus(id) {
-    const arr = cardsBasket.map((item) => {
-      if (item.id === id) {
-        item.number += 1;
-        return item;
-      }
-      return item;
-    });
-    setCardsBasket(arr);
-  }
-
+ 
   function getTotalSum() {
     let count = 0;
     basketJson.forEach((item) => {
       count += item.number * item.sum;
     });
-
     return count;
+   
   }
-  function getTotalQuantity() {
+  function getTotalQuantity(){
     let count = 0;
     basketJson.forEach((item) => {
       count += item.number;
     });
     return count;
   }
-
+ 
   return (
+    
     <div className={s.wr_sum}>
       <div className={s.common}>
         <h2 className={s.title}>Корзина</h2>
-        <div className={s.quantity}>{getTotalQuantity()}</div>
-      </div>
-      <div className={s.basket_wrap}>
-        <div className={s.wr_bag}>
-          {basketJson.map((element, id) => (
-            <div className={s.bags_cards} key={id}>
-              <div className={s.cont_img}>
-                {" "}
-                <img
-                  className={s.img}
-                  src={require(`../Main/images/${element.url}.png`)}
-                  alt="food"
-                ></img>
-              </div>
+        <div className={s.quantity}>
 
-              <div className={s.wr_bags_text}>
-                <p className={s.description}>{element.description}</p>
-                <p className={s.weight}>{element.weight}</p>
-                <p className={s.price}>{element.price}</p>
-              </div>
-              <div className={s.wr_bags_btn_container}>
-                <button
-                  onClick={() => {
-                    if (element.number > 0) minus(element.id);
-                  }}
-                  className={s.bags_btn}
-                >
-                  -
-                </button>
-                <span className={s.number}>{element.number}</span>
-                <button
-                  onClick={() => {
-                    plus(element.id);
-                  }}
-                  className={s.bags_btn}
-                >
-                  +
-                </button>
-              </div>
-            </div>
+         {getTotalQuantity()}
+        </div>
+      </div>
+      <div className={s.wr_bag}>
+        <div className={s.wr_bags_cards}>
+          {basket.map((element, index) => (
+            <ItemBasket basket={basket} setBasket={setBasket} object={element} key={index} />
           ))}
+        <div className={s.wr_sum}>
+          <div className={s.wr_sum_common}>
+            <span className={s.wr_sum_total}>
+              Итого
+              <span className={s.wr_sum_number}>
+              {getTotalSum()}₽
+              </span>
+            </span>
+          </div>
+          </div>
+          <button className={s.wr_sum_btn}>Оформить</button>
+          <div className={s.wr_delivery}>
+            <img src={delivery} alt="" className={s.delivery} />
+            <span className={s.wr_free}>Бесплатная доставка</span>
+          </div>
         </div>
-      </div>
-
-      <div className={s.wr_sum}>
-        <div className={s.wr_sum_common}>
-          <span className={s.wr_sum_total}>
-            Итого
-            <span className={s.wr_sum_number}>{getTotalSum()}₽</span>
-          </span>
-        </div>
-      </div>
-      <button className={s.wr_sum_btn}>Оформить</button>
-      <div className={s.wr_delivery}>
-        <img src={delivery} alt="" className={s.delivery} />
-        <span className={s.wr_free}>Бесплатная доставка</span>
       </div>
     </div>
+  
   );
 }
--- src/components/ItemBasket/ItemBasket.jsx
+++ src/components/ItemBasket/ItemBasket.jsx
@@ -1,67 +1,70 @@
-// import React from "react";
-// import s from "./ItemBasket.module.scss";
-// // import { useState } from "react";
-// // import Basket from "../Basket/Basket";
-// // import basketJson from "../data/basket.json";
+import React from "react";
+import s from "./ItemBasket.module.scss";
 
-// export default function ItemBasket(props) {
-//   console.log(props.object);
-  
-//   function minus(id) {
-//     const arr = props.object.basketJson.map((item) => {
 
-//       if (item.id === id ) {
-//         item.number -= 1;
-//         return item;
-//       }
-//       return item;
-//     });
-//     props.object.basketJson.setCardsBasket(arr);
-//   }
-//   function plus(id) {
-//     const arr =props.object.basketJson.map((item) => {
-//       if (item.id === id ) {
-//         item.number += 1;
-//         return item;
-//       }
-//       return item;
-//     });
-//     props.object.basketJson.setCardsBasket(arr);
+export default function ItemBasket(props) {
+
+  function delBasket(){
+    props.setBasket([])
+  }
+
+
+
+  function minus(id) {
+    const arr = props.basket.map((item) => {
+
+      if (item.id === id ) {
+        item.number -= 1;
+        return item;
+      }
+      return item;
+    });
+    props.setBasket(arr);
+  }
+  function plus(id) {
+    const arr = props.basket.map((item) => {
+      if (item.id === id ) {
+        item.number += 1;
+        return item;
+      }
+      return item;
+    });
+    props.setBasket(arr);
    
-   
-//   }
-//   return (
-//     <div className={s.basket_wrap}>
-//       <img
-//         className={s.img}
-//         src={require(`../Main/images/${props.object.url}.png`)}
-//         alt="food"
-//       />
-//       <div className={s.wr_bags_text}>
-//         <p className={s.description}>{props.object.description}</p>
-//         <p className={s.weight}>{props.object.weight}</p>
-//         <p className={s.price}>{props.object.price}</p>
-//       </div>
-//       <div className={s.wr_bags_btn_container}>
-//         <button
-//           onClick={() => {
-//             if(props.object.number>0)
-//             minus(props.object.id);
-//           }}
-//           className={s.bags_btn}
-//         >
-//           -
-//         </button>
-//         <span className={s.number}>{props.object.number}</span>
-//         <button
-//           onClick={() => {
-//             plus(props.object.id);
-//           }}
-//           className={s.bags_btn}
-//         >
-//           +
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
+  }
+  return (
+    <div className={s.basket_wrap}>
+      <img
+        className={s.img}
+        src={require(`../Main/images/${props.object.url}.png`)}
+        alt="food"
+      />
+      <div className={s.wr_bags_text}>
+        <p className={s.description}>{props.object.description}</p>
+        <p className={s.weight}>{props.object.weight}</p>
+        <p className={s.price}>{props.object.price}</p>
+      </div>
+      <div className={s.wr_bags_btn_container}>
+        <button
+          onClick={() => {
+            if(props.object.number>0)
+            minus(props.object.id)
+          }}
+         
+          className={s.bags_btn}
+        >
+          -
+        </button>
+        <span className={s.number}>{props.object.number}</span>
+        <button
+          onClick={() => {
+            plus(props.object.id);
+          }}
+          className={s.bags_btn}
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
